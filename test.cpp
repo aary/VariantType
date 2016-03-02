0ce3fb18c7af37b549fa5f6d3520c603b4b3e8c5
Aaryaman Sagar <aary@0587343075.wireless.umich.net>
2016-03-02 00:19:21 -0500

    Cleaned up test code

--- test.cpp
+++ test.cpp
@@ -1,30 +1,10 @@
-#include <set>
-using std::set;
-#include "VariantType.hpp"
-#include "VariantTypeImpl.hpp"
-#include <string>
-using std::string;
 #include <iostream>
-using std::cout;
-using std::endl;
 using std::cerr;
-#include <cassert>
 #include <stdexcept>
 using std::exception;
-using std::logic_error;
 
-template <typename ELEMENT>
-void print_element(ELEMENT ele) {
-    cout << ele << endl;
-}
-
-// Unit tests
+// run all unit tests
 extern void run_tests();
-extern void test_not_equals_operator();
-extern void test_less_than_operator();
-extern void test_greater_than_operator();
-extern void test_equals_operator();
-extern void test_copy_constructor();
 
 int main() {
 
