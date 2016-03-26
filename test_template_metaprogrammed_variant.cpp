62e230c007baa0c2e136e96409e89d9c62d63b17
Aaryaman Sagar <rmn100@gmail.com>
2016-03-26 00:38:53 -0400

    Added static assert to check whether the object can be constructed in a variant type of given template parameters

--- test_template_metaprogrammed_variant.cpp
+++ test_template_metaprogrammed_variant.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <cassert>
 #include <new>
 #include <iostream>
@@ -5,8 +6,11 @@
 #include <type_traits>
 using std::cout;
 using std::endl;
+using std::string;
 
 /* ******** Utilities ********* */
+// static_max function taken without any thought shamelessly from 
+//      https://gist.github.com/tibordp/6909880#file-variant-cc-L106
 template <size_t arg1, size_t ... others>
 struct static_max;
 template <size_t arg>
@@ -18,6 +22,8 @@ struct static_max<arg1, arg2, others...> {
     static const size_t value = arg1 >= arg2 ? static_max<arg1, others...>::value :
     static_max<arg2, others...>::value;
 };
+
+// taken from somewhere on stackoverflow
 template <typename... Vs>
 struct IndexInTemplatePack;
 // found it
@@ -29,6 +35,19 @@ struct IndexInTemplatePack<T, T, R...>
 template <typename T, typename F, typename... R>
 struct IndexInTemplatePack<T, F, R...> 
     : std::integral_constant<size_t, 1 + IndexInTemplatePack<T,R...>::value>
+{};
+
+// to check whether a type is included in a template parameter pack
+template <typename... Vs>
+struct IsTypeContainedIn { };
+template <typename ToCheck, typename Head, typename... Vs>
+struct IsTypeContainedIn<ToCheck, Head, Vs...> : 
+    public std::integral_constant<bool, false || 
+        IsTypeContainedIn<ToCheck, Vs...>::value>
+{};
+template <typename ToCheck, typename... Vs>
+struct IsTypeContainedIn<ToCheck, ToCheck, Vs...> : 
+    public std::integral_constant<bool, true>
 {};
 /* ******** Utilities ********* */
 
@@ -74,6 +93,10 @@ public:
 template <typename... Vs>
 template <typename Type>
 VariadicTemplatedType<Vs...>::VariadicTemplatedType(const Type& element) {
+
+    static_assert(IsTypeContainedIn<Type, Vs...>::value, 
+            "Cannot use this variant type with a type that is not included "
+            "in the parameter pack aka the template argument list");
 
     // construct the element in place and set the index
     new(&this->buffer) Type{element};
@@ -180,9 +203,10 @@ auto VariadicTemplatedType<Vs...>::apply_visitor(Visitor visitor) {
 
 int main() {
 
-    VariadicTemplatedType<int, double> temp {1.00};
+    VariadicTemplatedType<int, string> temp {string{"hello"}};
     temp.apply_visitor([](const auto& var) { cout << var << endl; });
-    cout << "type of temp is " << temp.current_type_index << endl;
+    cout << "index of the type type of temp is " << temp.current_type_index 
+        << endl;
 
     return 0;
 }
