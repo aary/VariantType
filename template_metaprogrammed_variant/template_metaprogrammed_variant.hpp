89727379f1e15dc85a21745f7d421bf03ec3001b
Aaryaman Sagar <rmn100@gmail.com>
2016-03-31 01:26:24 -0400

    Minor changes

--- template_metaprogrammed_variant/template_metaprogrammed_variant.hpp
+++ template_metaprogrammed_variant/template_metaprogrammed_variant.hpp
@@ -68,7 +68,10 @@ public:
      * The data that is going to be used as the buffer for this variant type,
      * i.e. any type included in the Vs... parameter pack can fit here.  This
      * will be the same size as a union that would contain variables of all
-     * the types listed in the parameter pack
+     * the types listed in the parameter pack.  There is no need for an
+     * explicit union here in this case, but we still have to deal with the
+     * downsides of POD types in an object oriented language.  placement new
+     * goes a long way in mitigating the damages
      */
     static const size_t data_size = static_max<sizeof(Vs)...>::value;
     typename std::aligned_union<data_size, Vs...>::type buffer;
